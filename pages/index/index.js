b5d49114ea1a812f1f1d4e205d5293647c3d803a
jaimezwalker <jaimezwalker@gmail.com>
2018-03-24 11:21:58 +0800

    refactor: forecast to hourlyWeather

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,7 @@ Page({
     nowTemp: '',
     nowWeather: '',
     nowWeatherBackground: "",
-    forecast: []
+    hourlyWeather: []
   },
   onLoad() {
     this.getNow()
@@ -51,19 +51,19 @@ Page({
           backgroundColor: weatherColorMap[weather],
         })
 
-        //set forecast
-        let forecast = []
+        //set hourlyWeather
+        let hourlyWeather = []
         let nowHour = new Date().getHours()
         for (let i = 0; i < 24; i += 3) {
-          forecast.push({
+          hourlyWeather.push({
             time: (i + nowHour) % 24 + "时",
             iconPath: '/images/sunny-icon.png',
             temp: "12°"
           })
         }
-        forecast[0].time = '现在'
+        hourlyWeather[0].time = '现在'
         this.setData({
-          forecast:forecast
+          hourlyWeather: hourlyWeather
         })
       },
       complete: () =>{
